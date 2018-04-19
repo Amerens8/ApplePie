d1d5bb13886783db03dbed6e363296d8b3186ba6
Amerens Jongsma <amerensj@gmail.com>
2018-04-19 14:17:07 +0200

    ApplePie update 2

---
+++ ApplePie/Game.swift
@@ -0,0 +1,37 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Amerens Geeske Jongsma on 19/04/2018.
+//  Copyright © 2018 Amerens Jongsma. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.characters.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word.characters {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += " _ "
+                
+            }
+            
+        }
+        return guessedWord
+    }
+}
+
+
